0a9dd62f9c16c67101a544427902d0f707cc983f
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-18 01:14:25 -0600

    feat: SectionHeader component added

---
+++ app/_components/SectionHeader/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classNames from "classnames/bind";
+import { Typography } from "@/app/_components"; // Asegúrate de tener este componente tipográfico
+import styles from "./SectionHeader.module.scss";
+
+const cx = classNames.bind(styles);
+
+interface SectionHeaderProps {
+  title: string;
+  subtitle?: string;
+  align?: "left" | "center" | "right"; // Controlar la alineación
+}
+
+const SectionHeader: React.FC<SectionHeaderProps> = ({
+  title,
+  subtitle,
+  align = "center", // Por defecto alinea en el centro
+}) => {
+  return (
+    <div className={cx("section-header", `section-header--${align}`)}>
+      <Typography variant="h3" className={cx("section-header__title")}>
+        {title}
+      </Typography>
+      {subtitle && (
+        <Typography variant="p1" className={cx("section-header__subtitle")}>
+          {subtitle}
+        </Typography>
+      )}
+    </div>
+  );
+};
+
+export default SectionHeader;
