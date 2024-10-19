fbd4d8272fdcbd5aaada81e3dc9d9c9011dbe6c2
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-19 02:07:54 -0600

    feat: TextArea component added

---
+++ app/_components/TextArea/index.tsx
@@ -0,0 +1,79 @@
+"use client";
+import React, { FC, TextareaHTMLAttributes, useState } from "react";
+import { useField } from "formik";
+import classNames from "classnames/bind";
+import styles from "./TextArea.module.scss";
+
+const cx = classNames.bind(styles);
+
+export interface TextAreaProps
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label: string | JSX.Element;
+  name: string;
+  variant?: "primary" | "secondary";
+  placeholder?: string;
+  labelClassName?: string; // Nueva prop para className del label
+  inputClassName?: string; // Nueva prop para className del input
+  errorClassName?: string; // Nueva prop para className del error
+}
+
+export const TextArea: FC<TextAreaProps> = ({
+  label,
+  name,
+  variant = "primary",
+  placeholder,
+  labelClassName,
+  inputClassName,
+  errorClassName,
+  ...props
+}) => {
+  const [field, meta] = useField(name);
+  const [focused, setFocused] = useState(false);
+
+  const containerClasses = cx("form-control", `form-control--${variant}`, {
+    "form-control--error": meta.touched && meta.error,
+    "form-control--not-empty": field.value,
+    "form-control--focused": focused,
+  });
+
+  const handleFocus = () => {
+    setFocused(true);
+  };
+
+  const handleBlur = () => {
+    setFocused(false);
+  };
+
+  return (
+    <div className={containerClasses}>
+      <div className={cx("form-control__input-container")}>
+        <label
+          className={cx("form-control__label", labelClassName, {
+            "form-control__label--focused": focused || field.value,
+          })}
+          htmlFor={name}
+        >
+          {label}
+        </label>
+
+        <textarea
+          className={cx("form-control__input", inputClassName)}
+          {...field}
+          {...props}
+          placeholder={variant === "primary" ? placeholder : ""}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          rows={8}
+        />
+
+        {meta.touched && meta.error && (
+          <div className={cx("form-control__error", errorClassName)}>
+            {meta.error}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TextArea;
