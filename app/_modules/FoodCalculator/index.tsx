f99273ea7a65498baf3e7c0074b2c4071d22c038
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-19 13:15:57 -0600

    feat: heading animation added

--- app/_modules/FoodCalculator/index.tsx
+++ app/_modules/FoodCalculator/index.tsx
@@ -1,18 +1,34 @@
 // FoodCalculator.tsx
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
 import classNames from "classnames/bind";
 import { SectionHeader, DogForm } from "@/app/_components";
 import dogIllustrationPrimary from "@/public/dog-illustration-primary.svg";
 import dogIllustrationSecondary from "@/public/dog-illustration-secondary.svg";
 import styles from "./FoodCalculator.module.scss";
+import useAnimations from "@/app/_hooks/useAnimations";
 
 const cx = classNames.bind(styles);
 
 const FoodCalculator: React.FC = () => {
+  const headingRef = useRef<HTMLDivElement>(null);
+
+  useAnimations([
+    {
+      ref: headingRef, // El ref del encabezado
+      type: "fadeInUp", // Tipo de animación (puedes usar cualquier tipo definido)
+      options: {
+        duration: 1.2, // Opciones específicas para la animación
+        ease: "power2.out",
+      },
+    },
+  ]);
+
   return (
     <section id="food-calculator" className={cx("food-calculator")}>
       <SectionHeader
+        ref={headingRef}
         title="Nutrición Ideal para tu Perro"
         subtitle="Encuentra la cantidad de alimento adecuada"
       />
