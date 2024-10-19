1bb751c7e05569774aaeb366398f1d5f90dc98ab
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-18 22:48:10 -0600

    feat: FoodCalculator module added to export

--- app/_modules/index.ts
+++ app/_modules/index.ts
@@ -2,5 +2,6 @@ import About from "./About";
 import Benefits from "./Benefits";
 import Hero from "./Hero";
 import ProductGrid from "./ProductGrid";
+import FoodCalculator from "./FoodCalculator";
 
-export { About, Benefits, Hero, ProductGrid };
+export { About, Benefits, Hero, ProductGrid, FoodCalculator };
