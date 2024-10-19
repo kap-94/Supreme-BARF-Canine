8419a3c0ebb41c0ff96c1ca0248fcd7c0c90cedb
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-19 00:32:25 -0600

    feat: FrequentAskedQuestions module added to barrel file

--- app/_modules/index.ts
+++ app/_modules/index.ts
@@ -3,5 +3,13 @@ import Benefits from "./Benefits";
 import Hero from "./Hero";
 import ProductGrid from "./ProductGrid";
 import FoodCalculator from "./FoodCalculator";
+import FrequentAskedQuestions from "./FrequentAskedQuestions";
 
-export { About, Benefits, Hero, ProductGrid, FoodCalculator };
+export {
+  About,
+  Benefits,
+  Hero,
+  ProductGrid,
+  FoodCalculator,
+  FrequentAskedQuestions,
+};
