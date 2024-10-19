f9a90a5b917e26776f40e5e845676495d49c4b00
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-19 02:22:31 -0600

    feat: useActiveStyles prop added in secondary menu for desktop

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -155,6 +155,7 @@ const Header: FC<HeaderProps> = ({
               frontPageID={1}
               data={menuDataSecondary}
               onClick={closeMenu}
+              useActiveStyle
               // frontPageID={frontPageID}
             />
           )}
