40ba54efabe55dfac78eece220638f370ed759fe
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-18 22:41:45 -0600

    feat: SectionHeader component added

--- app/_components/SectionHeader/index.tsx
+++ app/_components/SectionHeader/index.tsx
@@ -9,15 +9,19 @@ interface SectionHeaderProps {
   title: string;
   subtitle?: string;
   align?: "left" | "center" | "right"; // Controlar la alineación
+  className?: string;
 }
 
 const SectionHeader: React.FC<SectionHeaderProps> = ({
   title,
   subtitle,
   align = "center", // Por defecto alinea en el centro
+  className,
 }) => {
   return (
-    <div className={cx("section-header", `section-header--${align}`)}>
+    <div
+      className={cx("section-header", className, `section-header--${align}`)}
+    >
       <Typography variant="h3" className={cx("section-header__title")}>
         {title}
       </Typography>
