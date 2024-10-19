556dd6832c2d69007a79db513395a41063a90c29
Jose Pablo Karam <karamjp7@gmail.com>
2024-10-19 17:22:26 -0600

    feat: pregnancy field added to form

--- app/_components/DogForm/index.tsx
+++ app/_components/DogForm/index.tsx
@@ -18,6 +18,7 @@ const DogFormSchema = Yup.object().shape({
     .required("Este campo es requerido"),
   sterilized: Yup.string().required("Este campo es requerido"),
   activityLevel: Yup.string().required("Este campo es requerido"),
+  pregnancyStatus: Yup.string().required("Este campo es requerido"),
 });
 
 // Interfaz de los valores iniciales
@@ -27,6 +28,7 @@ interface DogFormValues {
   dogWeight: number | string;
   sterilized: string;
   activityLevel: string;
+  pregnancyStatus: string;
 }
 
 // Opciones para los Dropdowns (orden numérico)
@@ -41,14 +43,20 @@ const monthsOptions = Array.from({ length: 12 }, (_, i) => ({
 }));
 
 const sterilizedOptions = [
-  { value: "yes", label: "Sí" },
-  { value: "no", label: "No" },
+  { value: "Esterilizado", label: "Sí" },
+  { value: "No esterilizado", label: "No" },
 ];
 
 const activityLevelOptions = [
-  { value: "high", label: "Alta" },
-  { value: "medium", label: "Media" },
-  { value: "low", label: "Baja" },
+  { value: "Sedentario", label: "Baja" },
+  { value: "Moderado", label: "Media" },
+  { value: "Activo", label: "Alta" },
+];
+
+const pregnancyStatusOptions = [
+  { value: "Gestación", label: "Gestación" },
+  { value: "Lactancia", label: "Lactancia" },
+  { value: "No aplica", label: "No aplica" },
 ];
 
 const DogForm: React.FC = () => {
@@ -56,8 +64,9 @@ const DogForm: React.FC = () => {
     dogYears: "1", // Valor por defecto
     dogMonths: "0", // Valor por defecto
     dogWeight: "1", // Permitimos valor como string
-    sterilized: "yes", // Valor por defecto
-    activityLevel: "medium", // Valor por defecto
+    sterilized: "Esterilizado", // Valor por defecto
+    activityLevel: "Moderado", // Valor por defecto
+    pregnancyStatus: "No aplica", // Valor por defecto
   };
 
   const [calculationResult, setCalculationResult] = useState<{
@@ -191,6 +200,28 @@ const DogForm: React.FC = () => {
                       <div className={cx("error")}>{errors.activityLevel}</div>
                     )}
                   </div>
+
+                  {/* Dropdown para estado de gestación o lactancia */}
+                  <div className={cx("form-group")}>
+                    <Dropdown
+                      id="pregnancyStatus"
+                      label="Gestación o lactancia"
+                      selected={
+                        pregnancyStatusOptions.find(
+                          (option) => option.value === values.pregnancyStatus
+                        )!
+                      }
+                      onSelectedChange={(option) =>
+                        setFieldValue("pregnancyStatus", option.value)
+                      }
+                      options={pregnancyStatusOptions}
+                    />
+                    {errors.pregnancyStatus && touched.pregnancyStatus && (
+                      <div className={cx("error")}>
+                        {errors.pregnancyStatus}
+                      </div>
+                    )}
+                  </div>
                 </Form>
               );
             }}
@@ -200,16 +231,48 @@ const DogForm: React.FC = () => {
         {/* Resultados */}
         <div className={cx("dog-form__results")}>
           <Typography variant="h4">Cantidad de Alimento</Typography>
+
           <div className={cx("dog-form__result-details")}>
             {/* Renderizar foodAmount con HTML */}
             <p
-              dangerouslySetInnerHTML={{ __html: calculationResult.foodAmount }}
+              dangerouslySetInnerHTML={{
+                __html: calculationResult.foodAmount,
+              }}
             ></p>
 
             {/* Mensaje adicional permanece sin cambios */}
-            {calculationResult.additionalMessage && (
+            {/* {calculationResult.additionalMessage && (
               <p>{calculationResult.additionalMessage}</p>
-            )}
+            )} */}
+
+            {/* Sección de Tips */}
+            <div className={cx("tips-section")}>
+              <Typography variant="h5">
+                Consejos para una mejor alimentación
+              </Typography>
+              <br />
+              <ul className={cx("tips-list")}>
+                <li>
+                  <Typography variant="p2">
+                    Divide la cantidad diaria de alimento en 2 o 3 comidas para
+                    mejorar la digestión de tu perro.
+                  </Typography>
+                </li>
+                <li>
+                  <Typography variant="p1">
+                    Asegúrate de que tu perro siempre tenga acceso a agua fresca
+                    y limpia durante todo el día.
+                  </Typography>
+                </li>
+                <li>
+                  <Typography variant="p1">
+                    Consulta a tu veterinario regularmente para ajustar la
+                    cantidad de comida según las necesidades específicas de tu
+                    perro.
+                  </Typography>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
