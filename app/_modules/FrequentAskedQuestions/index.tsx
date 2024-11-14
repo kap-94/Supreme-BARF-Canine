d8d80a656aaa72b39ed20d8570682954c6724e62
Jose Pablo Karam <karamjp7@gmail.com>
2024-11-14 00:23:15 -0600

    feat: accordion data updated

--- app/_modules/FrequentAskedQuestions/index.tsx
+++ app/_modules/FrequentAskedQuestions/index.tsx
@@ -6,30 +6,41 @@ import styles from "./FrequentAskedQuestions.module.scss";
 
 const cx = classNames.bind(styles);
 
-// Datos para el componente Accordion
 const accordionData = {
   title: "Preguntas Frecuentes",
   subtitle: "Encuentra respuestas a las dudas más comunes",
   questions: [
     {
+      id: "natural-food",
+      title: "¿Por qué preferir un alimento natural?",
+      content: `Al igual que los humanos, nuestros amigos peludos también se benefician ampliamente de una alimentación más natural. Un alimento natural, como SUPREME, es más fresco y rico en nutrientes esenciales. Lo anterior, aunado a la ausencia de aditivos artificiales y harinas ultra procesadas, favorece una mejor digestión, piel y pelaje saludables, y más energía.`,
+    },
+    {
+      id: "cooked-vs-raw",
+      title: "¿Por qué preferir un alimento cocido a uno crudo?",
+      content: `Si bien los perros descienden de los lobos, lo cierto es que ambas especies se separaron hace millones de años y los perros llevan mucho tiempo ingiriendo alimentos con algún grado de cocción.
+      Sabemos que existen opciones crudas en el mercado, sin embargo, un alimento cocido, como SUPREME, ofrece importantes beneficios en contraste, como una mayor digestibilidad y seguridad al eliminar patógenos presentes en ingredientes crudos, evitando el riesgo de contaminación bacteriana y malos olores. También permite una mejor absorción de nutrientes específicos debido al proceso de cocción.`,
+    },
+    {
+      id: "can-eat-supreme",
+      title: "¿Mi perro puede comer Supreme BARF Canine?",
+      content: `¡Claro! Todos los perros son aptos para consumir SUPREME, las diferencias entre cada perro estarán en la porción diaria que deberás darle, dependiendo de factores como su peso, edad, nivel de actividad, esterilización y si se encuentra en etapa gestante o lactante. ¡Te invitamos a utilizar nuestra calculadora de alimento!`,
+    },
+    {
+      id: "how-to-buy",
+      title: "¿Cómo puedo comprar Supreme BARF Canine?",
+      content: `Puedes hacer tu pedido en nuestra tienda en línea o comunicarte con nosotros a través de WhatsApp. Una vez hecho tu pedido, pediremos que confirmes el pago y coordinaremos la entrega a domicilio de tu pedido. Verifica que tu ciudad se encuentre dentro de nuestro rango de entregas.
+      Aceptamos pagos con tarjeta (tienda en línea) y transferencias (comunícate a nuestro WhatsApp).`,
+    },
+    {
+      id: "returns-policy",
       title: "¿Cuál es la política de devoluciones?",
-      content: `Nuestra política de devoluciones dura 30 días. Si han pasado 30 días desde tu compra, lamentablemente no podemos ofrecerte un reembolso o cambio.`,
+      content: `Nuestra política de devoluciones dura 30 días en casos de algún defecto con el alimento. Si han pasado más de 30 días desde tu compra, lamentablemente no podemos ofrecerte un reembolso o cambio. Una vez que solicites el cambio o devolución, te pediremos que nos hagas entrega de la bolsa que tiene un defecto.`,
     },
     {
-      title: "¿Cómo puedo rastrear mi pedido?",
-      content: `Una vez que tu pedido haya sido enviado, recibirás un correo electrónico con un número de seguimiento. Puedes utilizar este número para rastrear tu pedido en el sitio web del transportista.`,
-    },
-    {
-      title: "¿Ofrecen envío internacional?",
-      content: `Sí, realizamos envíos a nivel internacional. Los tiempos de envío y costos pueden variar según el destino. Por favor, consulta nuestra página de envíos para más detalles.`,
-    },
-    {
-      title: "¿Cómo es el proceso de compra?",
-      content: `Para realizar una compra, simplemente agrega los productos que deseas al carrito y sigue los pasos del proceso de pago. Puedes registrarte para crear una cuenta o realizar la compra como invitado. Una vez confirmado el pago, recibirás un correo electrónico con los detalles de tu pedido.`,
-    },
-    {
-      title: "¿Qué métodos de pago aceptan?",
-      content: `Aceptamos varias formas de pago, incluyendo tarjetas de crédito y débito, PayPal y transferencias bancarias. Todas las transacciones son seguras y están encriptadas para proteger tu información.`,
+      id: "storage",
+      title: "¿Cómo conservar mi bolsa de SUPREME?",
+      content: `Por tratarse de un alimento natural, tus bolsas de SUPREME deberán permanecer en refrigeración en todo momento. Dentro del refrigerador, nuestras bolsas durarán, por lo menos, 30 días y si decides congelarlas, durarán aún más. Solo te sugerimos que las pongas en el refrigerador por lo menos un día antes de dárselas a tu peludo.`,
     },
   ],
   custom_anchor_id: "seccion-preguntas-frecuentes",
