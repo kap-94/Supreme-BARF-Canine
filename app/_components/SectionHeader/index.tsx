086f6ba1afd1e19750093281459a887f13ce3482
Jose Pablo Karam <karamjp7@gmail.com>
2024-11-14 18:32:59 -0600

    feat: SectionHeader color prop added and title font variant updated to h2

--- app/_components/SectionHeader/index.tsx
+++ app/_components/SectionHeader/index.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef } from "react";
 import classNames from "classnames/bind";
-import { Typography } from "@/app/_components"; // Asegúrate de tener este componente tipográfico
+import { Typography } from "@/app/_components";
 import styles from "./SectionHeader.module.scss";
 
 const cx = classNames.bind(styles);
@@ -8,23 +8,31 @@ const cx = classNames.bind(styles);
 interface SectionHeaderProps {
   title: string;
   subtitle?: string;
-  align?: "left" | "center" | "right"; // Controlar la alineación
+  align?: "left" | "center" | "right";
   className?: string;
+  color?: "white" | "black";
 }
 
-// Usamos forwardRef para permitir la referencia
 const SectionHeader = forwardRef<HTMLDivElement, SectionHeaderProps>(
-  ({ title, subtitle, align = "center", className }, ref) => {
+  ({ title, subtitle, align = "center", className, color = "black" }, ref) => {
     return (
       <div
-        ref={ref} // Pasar el ref al div contenedor
+        ref={ref}
         className={cx("section-header", className, `section-header--${align}`)}
       >
-        <Typography variant="h3" className={cx("section-header__title")}>
+        <Typography
+          variant="h2"
+          className={cx("section-header__title")}
+          color={color}
+        >
           {title}
         </Typography>
         {subtitle && (
-          <Typography variant="p1" className={cx("section-header__subtitle")}>
+          <Typography
+            variant="p1"
+            className={cx("section-header__subtitle")}
+            color={color}
+          >
             {subtitle}
           </Typography>
         )}
@@ -33,7 +41,6 @@ const SectionHeader = forwardRef<HTMLDivElement, SectionHeaderProps>(
   }
 );
 
-// Agrega un displayName para que el nombre del componente sea visible en herramientas de depuración
 SectionHeader.displayName = "SectionHeader";
 
 export default SectionHeader;
