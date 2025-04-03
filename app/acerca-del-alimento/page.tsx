9a88b15cd8b3ae32134f3c9e3fd2e1cb52d05127
Jose Pablo Karam <karamjp7@gmail.com>
2025-04-02 20:52:48 -0600

    refactor: comment out VeterinaryValidation component in AcercaDelAlimentoPage

--- app/acerca-del-alimento/page.tsx
+++ app/acerca-del-alimento/page.tsx
@@ -41,7 +41,7 @@ export default function AcercaDelAlimentoPage() {
         */}
         <About />
         <Benefits />
-        <VeterinaryValidation />
+        {/* <VeterinaryValidation /> */}
         <NutritionalData />
         <Contact />
       </main>
